fdc0f81565236121e2937025310641ba70fb35de
nobscamilo <juancamilo.sarmiento@gmail.com>
2025-12-06 22:38:44 +0100

    Actualizar app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -37,15 +37,9 @@ import {
   Dumbbell
 } from 'lucide-react';
 
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
   const firebaseConfig = {
     apiKey: "AIzaSyCX3euJ0bsl_wE0IiM-7KZL2i-B5IYKcdU",
     authDomain: "nutric-b5cb5.firebaseapp.com",
@@ -54,12 +48,8 @@ import {
     messagingSenderId: "891425941004",
     appId: "1:891425941004:web:258cb1cbaebfe53aeff029",
     measurementId: "G-91F2TN45DY"
-  };
+  
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
 };
 
 const app = initializeApp(firebaseConfig);
